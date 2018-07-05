a69e659bb663952f12755af755569efb4ee16420
shangyukyo <shangyukyo>
2018-07-05 17:07:15 +0800

    fixbug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :login?, :current_student, :set_title
-  # before_action :login_required
+  before_action :login_required
   
   def login_required    
     unless login?            
